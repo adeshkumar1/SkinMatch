865e0ee8747aa750b922e85abe43b7fcb9156608
anirudlappathi <anirud.lappathi@gmail.com>
2024-02-23 22:56:46 -0600

    Added camera to be added with Flask

--- src/Navigation.js
+++ src/Navigation.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 
 function Navigation() {
+<<<<<<< HEAD
     
     return (
         <div className='fbask-reg text-3xl border-b-black border-b-2'>
@@ -27,6 +28,31 @@ function Navigation() {
             </div>
         </div>
     );
+=======
+  return (
+    <div className='fbask-reg text-2xl border-b-black border-b-2'>
+        <div className='flex justify-between'>
+            <Link to="/">
+                <div>
+                    <button className='flex fbask-reg p-4 mx-4'>
+                        Skin <div className='fbask-ital'>Match</div>
+                    </button>
+                </div>
+            </Link>
+            <Link to="/aboutus">
+                <button className='p-4 mx-4'>
+                    About Us
+                </button>
+            </Link>
+            <Link to="/camera">
+                <button className='p-4 mx-4'>
+                    Camera
+                </button>
+            </Link>
+        </div>
+    </div>
+  );
+>>>>>>> 25c235bd (Added camera to be added with Flask)
 }
 
 export default Navigation;
