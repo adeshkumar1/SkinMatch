fe12d732f6220efc36e8f37102103673aa14338f
anirudlappathi <anirud.lappathi@gmail.com>
2024-02-24 00:39:14 -0600

    merged to get models

--- src/Navigation.js
+++ src/Navigation.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react';
 
 function Navigation() {
 <<<<<<< HEAD
+<<<<<<< HEAD
     
     return (
         <div className='fbask-reg text-3xl border-b-black border-b-2'>
@@ -29,6 +30,8 @@ function Navigation() {
         </div>
     );
 =======
+=======
+>>>>>>> d35ad87c (merged to get models)
   return (
     <div className='fbask-reg text-2xl border-b-black border-b-2'>
         <div className='flex justify-between'>
@@ -49,10 +52,38 @@ function Navigation() {
                     Camera
                 </button>
             </Link>
+=======
+    
+    return (
+        <div className='fbask-reg text-3xl border-b-black border-b-2'>
+            <div className='flex justify-between'>
+                <Link to="/">
+                    <div>
+                        <button className='flex fbask-reg p-4 mx-32'>
+                            Skin <div className='fbask-ital'>Match</div>
+                        </button>
+                    </div>
+                </Link>
+                <Link to="/aboutus">
+                    <button className='p-4 mx-32'>
+                        About Us
+                    </button>
+                </Link>
+                <Link to="/camera">
+                    <button className='p-4 mx-32'>
+                        Camera
+                    </button>
+                </Link>
+            </div>
+>>>>>>> 15168356 (Merges again)
         </div>
+<<<<<<< HEAD
     </div>
   );
 >>>>>>> 25c235bd (Added camera to be added with Flask)
+=======
+    );
+>>>>>>> d35ad87c (merged to get models)
 }
 
 export default Navigation;
