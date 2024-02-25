31f70bf55efda7d77c0fdb3365149b4d140e3273
anirudlappathi <anirud.lappathi@gmail.com>
2024-02-25 05:33:43 -0600

    fixed face_images

--- src/app.py
+++ src/app.py
@@ -23,7 +23,6 @@ def DownloadImages():
         image = request.files['face-scans']
         filename = request.headers.get('Content-Disposition').split('filename=')[1][1:-1]
         image.save(os.path.join(os.path.dirname(__file__), 'face_images', filename))
-        # faceData = imageInModels(image)
 
         return jsonify({
             "message": "Image Saved",
