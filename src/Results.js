968045ab932baca575ef91f53f2273f1cbc2da10
Adesh Kumar <89107712+adeshkumar1@users.noreply.github.com>
2024-02-25 05:15:08 -0600

    Final changes

--- src/Results.js
+++ src/Results.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./App.css";
 
 import { useEffect } from "react";
@@ -61,41 +60,6 @@ function Results() {
       </div>
     </div>
   );
-=======
-import './App.css';
-import Navigation from './Navigation';
-import ImageList from './ImageList';
-
-import { useLocation } from 'react-router-dom';
-import { useState } from 'react';
-
-function Results() {
-    
-    const location = useLocation();
-    const processingResults = location.state?.processingResults;
-    let pathImages = processingResults['paths'];
-    let summary = processingResults['summary'];
-
-    const [imagePaths, setImagePaths] = useState(pathImages);
-
-    console.log(pathImages);
-    console.log(summary);
-
-    return (
-        <div>
-            <Navigation />
-            <div class="flex justify-center text-8xl mt-16 text-center fbask-reg">
-                Results
-            </div>
-            <div>
-                <ImageList imagePaths={imagePaths} />
-            </div>
-            <div className='text-center'>
-                {summary}
-            </div>
-        </div>
-    );
->>>>>>> 2d40a058 (results changes)
 }
 
 export default Results;
