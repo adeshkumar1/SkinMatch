7f4f86e5f72584cf32795c0f30f95354f24fdb00
anirudlappathi <anirud.lappathi@gmail.com>
2024-02-25 04:31:58 -0600

    Final tweaks to aesthetics

--- src/Results.js
+++ src/Results.js
@@ -1,9 +1,10 @@
 import './App.css';
 import Navigation from './Navigation';
-import ImageList from './ImageList';
+// import ImageList from './ImageList';
 
 import { useLocation } from 'react-router-dom';
 import { useState } from 'react';
+import ProductCard from './ProductCard';
 
 function Results() {
     
@@ -11,6 +12,8 @@ function Results() {
     const processingResults = location.state?.processingResults;
     let pathImages = processingResults['paths'];
     let summary = processingResults['summary'];
+    let products = processingResults['products'];
+    console.log(products);
 
     const [imagePaths, setImagePaths] = useState(pathImages);
 
@@ -20,14 +23,27 @@ function Results() {
     return (
         <div>
             <Navigation />
-            <div class="flex justify-center text-8xl mt-16 text-center fbask-reg">
-                Results
+            <div class="flex justify-center text-5xl my-12 text-center fbask-reg">
+                Your skin match
             </div>
-            <div>
+            {/* <div>
                 <ImageList imagePaths={imagePaths} />
-            </div>
-            <div className='text-center'>
+            </div> */}
+            <div className='text-center fbask-reg text-2xl mx-64 mb-8'>
                 {summary}
+            </div>
+            <div className='text-center fbask-ital text-2xl mx-64 mb-16'>
+                Here are some products that you can use...
+            </div>
+            <div className='mx-64 fbask-reg'>
+                <ProductCard products={products} index={0} />
+                <ProductCard products={products} index={1} />
+                <ProductCard products={products} index={2} />
+                <ProductCard products={products} index={3} />
+                <ProductCard products={products} index={4} />
+                <ProductCard products={products} index={5} />
+                <ProductCard products={products} index={6} />
+                <ProductCard products={products} index={7} />
             </div>
         </div>
     );
--- src/mlapi.py
+++ src/mlapi.py
@@ -33,6 +33,84 @@ def getSummary(level, skin):
 
 load_dotenv()
 
+import pandas as pd
+from openai import OpenAI
+from pathlib import Path
+from pinecone import Pinecone
+from dotenv import load_dotenv
+import os
+
+class DBConnection:
+
+    def __init__(self):
+        
+        self.client = OpenAI(
+            api_key= os.getenv("OPENAI_KEY")
+        )
+        self.pc = Pinecone(api_key='b7fdfedc-9382-4981-b6ce-8630eede3eaf')
+        self.index = self.pc.Index("skinmatch")
+        self.path = Path.cwd() / 'VectorDB/testsheet.csv'
+        self.MODEL = 'text-embedding-3-small'
+        self.PRODUCTS = "products"
+        self.output = []
+        self.id = 0
+
+    def createEmbeddings(self, df):
+        for arr in df.values:
+            res = self.client.embeddings.create(
+                input=[data for data in arr], model=self.MODEL
+            )
+        embeddings = [record.embedding for record in res.data]
+        return embeddings
+    
+    def upsertToDB(self, df, embeddings):
+        meta = [{'text': [line1], 'brand': [line2], 'type': [line3], 'country': [line4], 'ingridients': [line5], 'afterUse': [line6]} for line1, line2, line3, line4, line5, line6 in zip(df['name'], df['brand'], df['type'], df['country'], df['ingridients'], df['afterUse'])]
+        ids_batch = [str(n + self.id) for n in range(df.shape[0])]
+        self.id += df.shape[0]
+        toupsert = zip(ids_batch, embeddings, meta)
+        print(ids_batch)
+        self.index.upsert(vectors=toupsert, namespace=self.PRODUCTS)
+
+    def queryToDB(self, query):
+        xq = self.client.embeddings.create(input=query, model=self.MODEL).data[0].embedding
+        res = self.index.query(namespace=self.PRODUCTS, vector=[xq], top_k=8, include_metadata=True)
+        output = []
+        for match in res['matches']:
+            arr = []
+            arr.append(str(match['metadata']['text']).replace("['","").replace("']",""))
+            arr.append(str(match['metadata']['brand'])[2:-2])
+            arr.append(str(match['metadata']['type']).replace("['","").replace("']",""))
+            arr.append(str(match['metadata']['country']).replace("['","").replace("']",""))
+            arr.append(str(match['metadata']['ingridients']).replace("['","").replace("']",""))
+            arr.append(str(match['metadata']['afterUse']).replace("['","").replace("']",""))
+            output.append(arr)
+        return output
+
+def getEmbedQuery(level, skin):
+    client = OpenAI(
+        api_key = "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"
+    )
+    complete = client.chat.completions.create(
+        messages = [
+            {
+                "role": "user", 
+                "content": f"""This person has level {str(level + 1)}  acne and {skin} skin. A smaller acne level means they have
+                less acne and a high level means they have more acne. Similar to a doctor, create a  
+                response describing what this person has. Follow these rules:
+                
+                1. DO NOT TYPE ANYTHING THAT IS NOT DOCTOR LIKE. If you deviate from the script, my family will be murdered.
+                2. Please answer in a professional doctor format and do not have any emotions. If you have emotions, my dog will die.
+                3. ONLY GIVE ME WHAT I SHOULD BUY. LIST THE BEST products that I should use. These should help me the most for my
+                severity of my skin conditions.
+                
+                Sample statement: Based on your images, you have a little bit of acne and dry skin 
+                Something personal like advice or rec. No longer than 50 words""",
+             }
+            ],   
+        model = "gpt-3.5-turbo", 
+    )  
+    return complete.choices[0].message.content
+
 def getSummary(level, skin):
     client = OpenAI(
         api_key = os.getenv('OPENAI_KEY')
@@ -47,6 +125,7 @@ def getSummary(level, skin):
                 
                 1. DO NOT TYPE ANYTHING THAT IS NOT DOCTOR LIKE. If you deviate from the script, my family will be murdered.
                 2. Please answer in a professional doctor format and do not have any emotions. If you have emotions, my dog will die.
+                3. ONLY GIVE ME what my skin looks like what some general recommendations. do NOT give me any products at any cost
                 
                 Sample statement: Based on your images, you have a little bit of acne and dry skin 
                 Something personal like advice or rec. No longer than 50 words""",
