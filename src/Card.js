e9e9fa39f49ee082eadd955d4a5a26929a37b560
Adesh Kumar <89107712+adeshkumar1@users.noreply.github.com>
2024-02-25 05:01:06 -0600

    About Us Page

--- src/Card.js
+++ src/Card.js
@@ -4,13 +4,14 @@ import Neil from "./Static/Neil.jpg";
 import Adesh from "./Static/Adesh.jpeg";
 import Theo from "./Static/Theodore.jpeg";
 import LinkedIn from "./Static/linkedin.svg";
+import GitHub from "./Static/github.svg"
 
 function Card() {
   return (
     <div className="w-3/4 mx-auto grid grid-cols-4 gap-16 mt-16">
       <div className="text-center">
         <img
-          className="rounded-full aspect-w-1 aspect-h-1 transform hover:scale-110 transition-transform"
+          className="border-black border-2 rounded-full aspect-w-1 aspect-h-1 transform hover:scale-110 transition-transform"
           src={Anirud}
           alt="Programmer Anirud"
         ></img>
@@ -18,14 +19,27 @@ function Card() {
         <h1 className="fbask-reg text-2xl mt-4">
           Hey! Im Anirud, a Computer Science + Crop Science student at UIUC!
         </h1>
-        <div className="">
+        <div className="w-1/3 mx-auto grid grid-cols-2 ">
           <a
             href="https://www.linkedin.com/in/anirudl/"
             target="_blank"
-            className="flex justify-between" rel="noreferrer"
+            className="flex justify-between"
+            rel="noreferrer"
           >
             <img
               src={LinkedIn}
+              className="mx-auto mt-2 aspect-w-1 aspect-h-1 transform hover:scale-110 transition-transform"
+              alt=""
+            ></img>
+          </a>
+          <a
+            href="https://github.com/anirudlappathi"
+            target="_blank"
+            className="flex justify-between"
+            rel="noreferrer"
+          >
+            <img
+              src={GitHub}
               className="mx-auto mt-2 aspect-w-1 aspect-h-1 transform hover:scale-110 transition-transform"
               alt=""
             ></img>
@@ -34,7 +48,7 @@ function Card() {
       </div>
       <div className="text-center">
         <img
-          className="rounded-full aspect-w-1 aspect-h-1 transform hover:scale-110 transition-transform"
+          className="border-black border-2 rounded-full aspect-w-1 aspect-h-1 transform hover:scale-110 transition-transform"
           src={Neil}
           alt="Programmer Neil"
         ></img>
@@ -43,39 +57,27 @@ function Card() {
           Wassup! Im Neil, a Computer Science student passionate in AI/ML at
           UIUC!
         </h1>
-        <div className="">
+        <div className="w-1/3 mx-auto grid grid-cols-2 ">
           <a
             href="https://www.linkedin.com/in/neil-ganguly/"
             target="_blank"
-            className="flex justify-between" rel="noreferrer"
+            className="flex justify-between"
+            rel="noreferrer"
           >
             <img
               src={LinkedIn}
-              className="mx-auto mt-2 aspect-w-1 aspect-h-1 transform hover:scale-110"
+              className="mx-auto mt-2 aspect-w-1 aspect-h-1 transform hover:scale-110 transition-transform"
               alt=""
             ></img>
           </a>
-        </div>
-      </div>
-      <div className="text-center">
-        <img
-          className="rounded-full aspect-w-1 aspect-h-1 transform hover:scale-110 transition-transform"
-          src={Adesh}
-          alt="Programmer Adesh"
-        ></img>
-
-        <h1 className="fbask-reg text-2xl mt-4">
-          Greetings! Im Adesh, a passionate Computer Science student here at
-          UIUC!
-        </h1>
-        <div className="">
           <a
-            href="https://www.linkedin.com/in/adesh-kumar2/"
+            href="https://github.com/nganguly0594"
             target="_blank"
-            className="flex justify-between" rel="noreferrer"
+            className="flex justify-between"
+            rel="noreferrer"
           >
             <img
-              src={LinkedIn}
+              src={GitHub}
               className="mx-auto mt-2 aspect-w-1 aspect-h-1 transform hover:scale-110 transition-transform"
               alt=""
             ></img>
@@ -84,7 +86,45 @@ function Card() {
       </div>
       <div className="text-center">
         <img
-          className="rounded-full aspect-w-1 aspect-h-1 transform hover:scale-110 transition-transform"
+          className="border-black border-2 rounded-full aspect-w-1 aspect-h-1 transform hover:scale-110 transition-transform"
+          src={Adesh}
+          alt="Programmer Adesh"
+        ></img>
+
+        <h1 className="fbask-reg text-2xl mt-4">
+          Greetings! Im Adesh, a passionate Computer Science student here at
+          UIUC!
+        </h1>
+        <div className="w-1/3 mx-auto grid grid-cols-2 ">
+          <a
+            href="https://www.linkedin.com/in/adesh-kumar2/"
+            target="_blank"
+            className="flex justify-between"
+            rel="noreferrer"
+          >
+            <img
+              src={LinkedIn}
+              className="mx-auto mt-2 aspect-w-1 aspect-h-1 transform hover:scale-110 transition-transform"
+              alt=""
+            ></img>
+          </a>
+          <a
+            href="https://github.com/adeshkumar1"
+            target="_blank"
+            className="flex justify-between"
+            rel="noreferrer"
+          >
+            <img
+              src={GitHub}
+              className="mx-auto mt-2 aspect-w-1 aspect-h-1 transform hover:scale-110 transition-transform"
+              alt=""
+            ></img>
+          </a>
+        </div>
+      </div>
+      <div className="text-center">
+        <img
+          className="border-black border-2 rounded-full aspect-w-1 aspect-h-1 transform hover:scale-110 transition-transform"
           src={Theo}
           alt="Programmer Theodore"
         ></img>
@@ -92,14 +132,27 @@ function Card() {
         <h1 className="fbask-reg text-2xl mt-4">
           Salutations! Im Theodore, and I'm interested in Comp E at UIUC!
         </h1>
-        <div className="">
+        <div className="w-1/3 mx-auto grid grid-cols-2 ">
           <a
             href="https://www.linkedin.com/in/theodore-ng/"
             target="_blank"
-            className="flex justify-between" rel="noreferrer"
+            className="flex justify-between"
+            rel="noreferrer"
           >
             <img
               src={LinkedIn}
+              className="mx-auto mt-2 aspect-w-1 aspect-h-1 transform hover:scale-110 transition-transform"
+              alt=""
+            ></img>
+          </a>
+          <a
+            href="https://github.com/theodoreng15"
+            target="_blank"
+            className="flex justify-between"
+            rel="noreferrer"
+          >
+            <img
+              src={GitHub}
               className="mx-auto mt-2 aspect-w-1 aspect-h-1 transform hover:scale-110 transition-transform"
               alt=""
             ></img>
