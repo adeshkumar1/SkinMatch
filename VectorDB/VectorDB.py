bbe5b93448f03e8505238d2fdfe2f0d763c544ed
Adesh Kumar <89107712+adeshkumar1@users.noreply.github.com>
2024-02-25 00:47:10 -0600

    finishing touches to VectorDB

--- VectorDB/VectorDB.py
+++ VectorDB/VectorDB.py
@@ -1,59 +1,62 @@
 import pandas as pd
-from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAI
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain
+from openai import OpenAI
 from pathlib import Path
-from langchain_openai import OpenAIEmbeddings
-import csv
-import chromadb
+from pinecone import Pinecone
+from dotenv import load_dotenv
+import os
+load_dotenv()
+#----------------------------------------------------------------------
+class DBConnection:
 
-client = chromadb.PersistentClient(path='/Users/adesh/Desktop/Hackathon/hackathon/VectorDB')
-client.heartbeat()
+    def __init__(self):
+        
+        self.client = OpenAI(
+            api_key= os.getenv("OPENAI_KEY")
+        )
+        self.pc = Pinecone(api_key='b7fdfedc-9382-4981-b6ce-8630eede3eaf')
+        self.index = self.pc.Index("skinmatch")
+        self.path = Path.cwd() / 'VectorDB/testsheet.csv'
+        self.MODEL = 'text-embedding-3-small'
+        self.PRODUCTS = "products"
+        self.embeddings = []
+        self.output = []
 
-def getVectorDB(path):
-    print("--------------------")
-    df = pd.read_csv(path)
+    def createEmbeddings(self, df):
+        for arr in df.values:
+            res = self.client.embeddings.create(
+                input=[data for data in arr], model=self.MODEL
+            )
+        self.embeddings = [record.embedding for record in res.data]
+    
+    def upsertToDB(self, df):
+        meta = [{'text': [line1], 'brand': [line2], 'type': [line3], 'country': [line4], 'ingridients': [line5], 'afterUse': [line6]} for line1, line2, line3, line4, line5, line6 in zip(df['name'], df['brand'], df['type'], df['country'], df['ingridients'], df['afterUse'])]
+        ids_batch = [str(n) for n in range(df.shape[0])]
+        toupsert = zip(ids_batch, self.embeddings, meta)
+        self.index.upsert(vectors=toupsert, namespace=self.PRODUCTS)
 
+    def queryToDB(self, query):
+        xq = self.client.embeddings.create(input=query, model=self.MODEL).data[0].embedding
+        res = self.index.query(namespace=self.PRODUCTS, vector=[xq], top_k=5, include_metadata=True)
+        for match in res['matches']:
+            arr = []
+            arr.append(str(match['metadata']['text']).replace("['","").replace("']",""))
+            arr.append(str(match['metadata']['brand'])[2:-2])
+            arr.append(str(match['metadata']['type']).replace("['","").replace("']",""))
+            arr.append(str(match['metadata']['country']).replace("['","").replace("']",""))
+            arr.append(str(match['metadata']['ingridients']).replace("['","").replace("']",""))
+            arr.append(str(match['metadata']['afterUse']).replace("['","").replace("']",""))
+            self.output.append(arr)
+        return self.output
+
+if __name__ == "__main__":
+
+    df = pd.read_csv(Path.cwd() / 'VectorDB/testsheet.csv')
     df = df.dropna(axis=0)
-    df = df[:5]
 
-    new_list = []
-    for index, row in df.iterrows():
-        string = ''
-    for col in df.columns:
-        string += str(row[col]) + ','
-    new_list.append(string)
-    vectordb = Chroma.from_texts(new_list, OpenAIEmbeddings(model="text-embedding-3-large", openai_api_key='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'))
-    return vectordb
+    dbc = DBConnection()
+    dbc.createEmbeddings(df)
+    dbc.upsertToDB(df)
+    print(dbc.queryToDB("I have oily skin can you reccomend me products"))
 
-def getQuery(query, vectordb):
-    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-    qa = ConversationalRetrievalChain.from_llm(llm=OpenAI(openai_api_key='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'),
-                                            memory=memory,
-                                            retriever=vectordb.as_retriever())
-    brand = []
-    name = []
+    
 
-    qa.run(query)
-
-    docs_and_scores = vectordb.similarity_search_with_score(query)
-    for i in docs_and_scores:
-        string = str(i).split('\'')[1]
-        array = string.split(',')
-        brand.append(array[0])
-        name.append(array[1])
-
-    output = "Here are some products you can try 😁 \n"
-
-    for i in range(len(brand)):
-        output += name[i] + ' from ' + brand[i] + "\n"
-
-    return output
-
-path = Path.cwd() / 'VectorDB/datasheet.csv'
-vectordb = getVectorDB(path)
-
-
-for i in range(5):
-    print(getQuery("I have oily skin can you recoomend me some products", vectordb))
