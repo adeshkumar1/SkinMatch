58183746128c070adc3ad1ca3e10ad5f160eb2ce
anirudlappathi <anirud.lappathi@gmail.com>
2024-02-26 16:19:21 -0600

    Final Changes

--- src/mlapi.py
+++ src/mlapi.py
@@ -9,31 +9,6 @@ import os
 
 load_dotenv()
 
-def getSummary(level, skin):
-    client = OpenAI(
-        api_key = os.getenv('OPENAI_KEY')
-    )
-    complete = client.chat.completions.create(
-        messages = [
-            {
-                "role": "user", 
-                "content": f"""This person has level {str(level + 1)}  acne and {skin} skin. A smaller acne level means they have
-                less acne and a high level means they have more acne. Similar to a doctor, create a  
-                response describing what this person has. Follow these rules:
-                
-                1. DO NOT TYPE ANYTHING THAT IS NOT DOCTOR LIKE. If you deviate from the script, my family will be murdered.
-                2. Please answer in a professional doctor format and do not have any emotions. If you have emotions, my dog will die.
-                
-                Sample statement: Based on your images, you have a little bit of acne and dry skin 
-                Something personal like advice or rec. No longer than 50 words""",
-             }
-            ],   
-        model = "gpt-3.5-turbo", 
-    )  
-    return complete.choices[0].message.content
-
-load_dotenv()
-
 import pandas as pd
 from openai import OpenAI
 from pathlib import Path
@@ -103,30 +78,6 @@ def getEmbedQuery(level, skin):
     )  
     return complete.choices[0].message.content
 
-def getSummary(level, skin):
-    client = OpenAI(
-        api_key = os.getenv('OPENAI_KEY')
-    )
-    complete = client.chat.completions.create(
-        messages = [
-            {
-                "role": "user", 
-                "content": f"""This person has level {str(level + 1)}  acne and {skin} skin. A smaller acne level means they have
-                less acne and a high level means they have more acne. Similar to a doctor, create a  
-                response describing what this person has. Follow these rules:
-                
-                1. DO NOT TYPE ANYTHING THAT IS NOT DOCTOR LIKE. If you deviate from the script, my family will be murdered.
-                2. Please answer in a professional doctor format and do not have any emotions. If you have emotions, my dog will die.
-                3. ONLY GIVE ME what my skin looks like what some general recommendations. do NOT give me any products at any cost
-                
-                Sample statement: Based on your images, you have a little bit of acne and dry skin 
-                Something personal like advice or rec. No longer than 50 words""",
-             }
-            ],   
-        model = "gpt-3.5-turbo", 
-    )  
-    return complete.choices[0].message.content
-
 def livefeed():
     model = YOLO(Path.cwd() / 'src/model/AcneYOLO.pt')
     cap = cv2.VideoCapture(1)
@@ -180,6 +131,53 @@ def imageRatings(list_of_images):
     final_type = labels[type_num]
 
     return final_grade, final_type
+
+def getSummary(level, skin):
+    client = OpenAI(
+        api_key = os.getenv('OPENAI_KEY')
+    )
+    complete = client.chat.completions.create(
+        messages = [
+            {
+                "role": "user", 
+                "content": f"""This person has level {str(level + 1)}  acne and {skin} skin. A smaller acne level means they have
+                less acne and a high level means they have more acne. Similar to a doctor, create a  
+                response describing what this person has. Follow these rules:
+                
+                1. DO NOT TYPE ANYTHING THAT IS NOT DOCTOR LIKE.
+                2. Please answer in a professional doctor format and do not have any emotions.
+                3. ONLY GIVE ME what my skin looks like what some general recommendations. do NOT give me any products at any cost
+                
+                Sample statement: Based on your images, you have a little bit of acne and dry skin 
+                Something personal like advice or rec. No longer than 50 words""",
+             }
+            ],   
+        model = "gpt-3.5-turbo", 
+    )  
+    return complete.choices[0].message.content
+
+def getSummary(level, skin):
+    client = OpenAI(
+        api_key = os.getenv('OPENAI_KEY')
+    )
+    complete = client.chat.completions.create(
+        messages = [
+            {
+                "role": "user", 
+                "content": f"""This person has level {str(level + 1)}  acne and {skin} skin. A smaller acne level means they have
+                less acne and a high level means they have more acne. Similar to a doctor, create a  
+                response describing what this person has. Follow these rules:
+                
+                1. DO NOT TYPE ANYTHING THAT IS NOT DOCTOR LIKE.
+                2. Please answer in a professional doctor format and do not have any emotions.
+                
+                Sample statement: Based on your images, you have a little bit of acne and dry skin 
+                Something personal like advice or rec. No longer than 50 words""",
+             }
+            ],   
+        model = "gpt-3.5-turbo", 
+    )  
+    return complete.choices[0].message.content
 
 if __name__ == "__main__":
     livefeed()
